2dd8793b04f0d4947ed8fa1215c9a5f64fec72e5
Utensile <132955875+Utensile@users.noreply.github.com>
2023-06-12 23:43:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp utensile
+ File C & C++
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string ans;
+    cout << "What's up?" << endl;
+    cin >> ans;
+    cout << "Ah so you're " << ans << endl;
+}
